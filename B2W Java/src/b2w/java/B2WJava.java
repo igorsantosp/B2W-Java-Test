e85725c451cb01ff95e68dc2afce46bcce9d07f4
João Igor dos Santos Pereira <igorsantos95@live.com>
2021-04-04 20:23:08 -0300

    Comentários e Simplificações de código
    Criei um README explicando o processo de resolução do problema, fiz algumas simplificações no código e adicionei comentários explicando-o.

--- B2W Java/src/b2w/java/B2WJava.java
+++ B2W Java/src/b2w/java/B2WJava.java
@@ -19,44 +19,42 @@ public class B2WJava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Digite o número que deseja obter as combinações sem zeros à esquerda");
-        Scanner leia = new Scanner(System.in);
-        int numero = leia.nextInt();
-        String numeros = String.valueOf(numero);
-        String numeros2 = numeros;
-        int tamanho = numeros.length();
-        int zeros = 0;
-        int repetidos = 0;
-        for (int i = 0; i < tamanho; i++) {
+        Scanner leia = new Scanner(System.in);                                                          
+        int numero = leia.nextInt();                                            // Entrada: Número qual se quer descobrir as combinações possíveis sem zeros à esquerda
+        String numeros = String.valueOf(numero);                                // Transformando a entrada em String
+        String numeros2 = numeros;                                              // Cópia da entrada
+        int tamanho = numeros.length();                                         // Obtendo a quantidade total de digitos
+        int zeros = 0;                                                          // Variável que armazenará a quantidade total de zeros
+        int repetidos = 0;                                                      // Variável que armazenará a quantidade de números repetidos                                                                
+        for (int i = 0; i < tamanho; i++) {                                     // Loop que conta a quantidade de zeros e números repetidos
             if (numeros.charAt(i) == '0') {
-                zeros++;
+                zeros++;                                                        // Se o catactere atual for '0' incrementa o contador de zeros 
             } else {
                 if (numeros2.contains(String.valueOf(numeros.charAt(i)))) {
-                    if (numeros2.indexOf(numeros2.charAt(i)) != i) {
-                        repetidos++;
-                        /* if(i>0){
-                    numeros2= numeros2.substring(0, i)+"a"+numeros2.substring(i+1, numeros2.length());
-                    }else{
-                    numeros2= numeros2.substring(0, i)+"a"+numeros2.substring(i+1, numeros2.length()-1);
-                    }*/
+                    if (numeros2.indexOf(numeros2.charAt(i)) != i) {            // Se o caractere atual não for '0' e há uma correspondencia dele num index diferente do atual,
+                        repetidos++;                                            // repetidos é incrementado
                     }
                 }
             }
         }
-        int combinacao = repetidos == 0 ? tamanho - zeros : tamanho - zeros - (repetidos);
-        if (combinacao > 1 || repetidos > 0) {
-            if (zeros >= 1) {
-                for (int i = tamanho - 1; i >= zeros; i--) {
+                                                                                // Nesse ponto do código já temos as informações da quantidade de digitos, zeros e números repetidos
+                                                                                // Com essas informações abstraimos uma formula baseada em permutação matemática
+        int combinacao = tamanho - zeros - repetidos;                           // O primeiro termo do "fatorial" que nos dá a resposta é sempre tamanho - quantidade de zeros - quantidade de números repetidos
+        if (combinacao > 1 || (repetidos > 0 && repetidos<tamanho-1)) {
+            if (zeros >= 1) {                                                   // A quantidade de iterações para quando o numero tem algum digito 0 é diferente para quando não tem, por isso separei em ifs e loops diferentes
+                for (int i = tamanho - 1; i >= zeros; i--) {                    // Iteração correspondente à quando há zeros no número (tamanho-zeros-repetidos)*(tamanho-1) ...
                     combinacao *= i;
                     i--;
                 }
             } else {
-                for (int i = tamanho - 1; i >= 1; i--) {
-                    combinacao *= i;
+                for (int i = tamanho - 1; i >= 1; i--) {                        //Quando não há zeros, a itereção do fatorial comum deve se dar normalmente, alterando apenas o primeiro termo a depender de quanto numeros repetidos há, (tamanho-repetidos)*(tamanho-1)*(tamanho-2)... 
+                    combinacao *= i;                                            
                     i--;
                 }
             }
         }
-        System.out.println(String.valueOf(combinacao));
+        System.out.println(String.valueOf(combinacao));                         // Resposta final equivalente à quantidades de números diferentes gerados pela entrada, desconsiderando combinações com números iguais e números com 0 à esquerda
     }
     
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # B2W-Java-Test
 Teste de Lógica de Programação em Java
+
+# Comando da questão:
+You need to count the numbers that can be created by rearranging the digits of a positive Integer N (0<N<1000000000 and it has no leading zeros).
+Example: There are 6 numbers that can be created by rearranging the digits of 432 and there are 4 numbers that can be formed from the number 120 (you must disconsider numbers with leading zeros).
+The code must be written in Java 8.
+
+Em resumo, contar as combinações possíveis de números com os dígitos de um dado número de entrada. Números com 0 à esquerda devem ser desconsiderados.
+
+# Resolução:
+Uma das formas de resolver parece ser encontrar uma variação de fórmula simples de análise combinatória, por tanto conclui que pensando em alguma fórmula de análise combinatória conseguiria resolver a questão sem precisar necessariamente gerar todas as combinações possíveis e excluir os casos não desejados, já que, dependendo da quantidade de dígitos no número entrada, gerar todas as combinações possíveis pode requerer um processamento muito alto, tornando o algoritmo não ótimo.
+A quantidade de combinações dada por uma entrada em que todos os números são diferentes entre si e diferentes de 0 é dada por N! (fatorial simples) sendo N a quantidade de dígitos no número. A partir desta fórmula simples tentei generalizar uma fórmula para quando apareçam zeros e números iguais dentre os dígitos do número de entrada.
+Percebi que a quantidade de zeros e número iguais impacta diretamente no primeiro termo do fatorial e na quantidade de iterações que o fatorial deve realizar para retornar a resposta correta.
+Percebi que o primeiro termo do fatorial é sempre representado por: **Número de dígitos total - Número de zeros - Quantidade de Repetições de número**
+No caso de não haver zeros ou números repetidos, o primeiro termo do fatorial é sempre igual à quantidade de dígitos no número.
+A partir dessa observação criei um código que consegue contar a quantidade de dígitos e de zeros no número de entrada, além da quantidade de vezes que acontece uma repetição de qualquer número, com essas informações o código pode tomar dois tipos de loops diferentes. Um para fatorial simples e outro para quando há zeros.
+
+## Entradas testadas:
+120   (Três dígitos e um zero). 
+432   (Três dígitos, sem zeros ou repetições).
+1000  (Quatro dígitos, três zeros).
+1200  (Quatro dígitos, dois zeros).
+12000 (Cinco dígitos, três zeros).
+11000 (Cinco dígitos, dois zeros e dois dígitos repetidos).
+1122  (Quatro dígitos, duas repetições).
+1111  (Quatro dígitos, três repetições).
+
+Todos os testes retornaram a resposta esperada.
+Utilizei variáveis do tipo int já que em java o valor máximo armazenado ultrapassa dois bilhões, a entrada é prevista de ser menor que um bilhão e o maior número de combinações possível é 10!=3.628.800
